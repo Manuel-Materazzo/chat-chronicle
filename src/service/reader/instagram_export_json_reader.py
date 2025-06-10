1f44d2b64a3bd64c9e38ddb7d320f34d70eafa4c
Manuel <manuelmaterazzo@gmail.com>
2025-06-10 15:12:39 +0200

    instagram export specialized json reader

---
+++ src/service/reader/instagram_export_json_reader.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+
+from src.dto.instagram_export_message import InstagramExportMessage
+from src.dto.message import Message
+from src.service.logging_service import LoggingService
+from src.service.reader.json_reader import JsonReader
+
+
+class InstagramExportJsonReader(JsonReader):
+
+    def __init__(self, system_messages: dict, logging_service: LoggingService):
+        super().__init__(logging_service)
+        # get messages from configs
+        self.message_like = system_messages.get("user-interactions", {}).get("message-like", "")
+        self.message_reaction = system_messages.get("user-interactions", {}).get("message-reaction", "Added reaction")
+        self.message_reel = system_messages.get("user-content", {}).get("posts-and-reels", "[Shared an internet video]")
+        self.message_video = system_messages.get("user-content", {}).get("video-uploads", "[Sent a video of himself]")
+        self.message_photo = system_messages.get("user-content", {}).get("photo-uploads", "[Sent a photo of himself]")
+        self.message_audio = system_messages.get("user-content", {}).get("audio-messages", "[Sent an audio message]")
+
+    def standardize_messages(self, lines: dict) -> list[Message]:
+        messages: list[Message] = []
+
+        for raw_message in lines.get("messages", []):
+            # compute timestamp
+            timestamp = datetime.fromtimestamp(raw_message.get("timestamp_ms", 1000) / 1000.0)
+
+            content = self.__get_message_content(raw_message)
+            sender = raw_message.get("sender_name", "unknown")
+            messages.append({
+                'sender_name': sender,
+                'timestamp': timestamp,
+                'content': content,
+            })
+
+        return messages
+
+    def __get_message_content(self, raw_message: InstagramExportMessage) -> str:
+        """
+        Gets a content for
+        TODO: add reference for reactions?
+        TODO: transcribe audio files?
+        TODO: handle message editing
+        :param raw_message:
+        :return:
+        """
+
+        # handle reels
+        if raw_message.get("share", None) is not None:
+            return self.message_reel
+        # handle videos
+        if raw_message.get("videos", None) is not None:
+            return self.message_video
+        # handle photos
+        if raw_message.get("photos", None) is not None:
+            return self.message_photo
+        # handle audios TODO: transcribe?
+        if raw_message.get("audio_files", None) is not None:
+            return self.message_audio
+
+        content = raw_message.get("content", "")
+
+        # handle message likes and reactions
+        if content == self.message_like or self.message_reaction in content:
+            return ""
+
+        return content
