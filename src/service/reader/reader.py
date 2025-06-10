a689062ac32e584b82da0bf4e46e9c67688aba6f
Manuel <manuelmaterazzo@gmail.com>
2025-06-10 14:34:03 +0200

    reader interface

---
+++ src/service/reader/reader.py
@@ -0,0 +1,42 @@
+import os
+from abc import ABC, abstractmethod
+
+from src.service.logging_service import LoggingService
+
+
+class Reader(ABC):
+
+    def __init__(self, logging_service: LoggingService, extension: str):
+        self.extension = extension
+        self.logger = logging_service.get_logger(__name__)
+
+    def get_extension(self):
+        return self.extension
+
+    def get_file_list(self, input_directory: bytes):
+        """
+        Get a list of all useful files in the input directory.
+        :param input_directory:
+        :return:
+        """
+        files = []
+        for file in os.listdir(input_directory):
+            filename = os.fsdecode(file)
+            self.logger.debug(f'File or folder found: {filename}')
+            if filename.lower().endswith(self.extension):
+                self.logger.debug(f'Saving it as: {input_directory}/{filename}')
+                files.append(f"{input_directory}/{filename}")
+
+        if len(files) == 0:
+            raise FileNotFoundError(f"No {self.extension} files found on {input_directory}")
+        else:
+            self.logger.info(f'Found {len(files)} files to process...')
+
+        return files
+
+    @abstractmethod
+    def read(self, path: str):
+        """
+        Reads the provided file
+        :return:
+        """
