485755dd30d6b08a1c9290de7b8919bbf85ef8a5
Manuel <manuelmaterazzo@gmail.com>
2025-06-10 15:42:41 +0200

    update parser factory with new parser signatures

--- src/service/parser/parser_factory.py
+++ src/service/parser/parser_factory.py
@@ -4,11 +4,10 @@ from src.service.parser.parser import Parser
 from src.service.parser.whatsapp_export import WhatsappExport
 
 
-def parser_factory(config: dict, paths: list[str]) -> Parser:
+def parser_factory(config: dict) -> Parser:
     """
     Instantiates the parser for the provided file type from the dictionary and returns it.
     :param config: Dictionary containing the configuration of the application.
-    :param paths: List of file paths.
     :return:
     """
     # get configs
@@ -16,16 +15,15 @@ def parser_factory(config: dict, paths: list[str]) -> Parser:
     chat_sessions_enabled = config.get('parsing', {}).get('chat-sessions', {}).get('enabled', True)
     sleep_window_start = config.get('parsing', {}).get('chat-sessions', {}).get('sleep-window-start-hour', 2)
     sleep_window_end = config.get('parsing', {}).get('chat-sessions', {}).get('sleep-window-end-hour', 9)
-    system_messages = config.get('parsing', {}).get('messages', {})
     ignore_chat_enabled = config.get('parsing', {}).get('ignore-chat', {}).get('enabled', False)
     ignore_chat_before = config.get('parsing', {}).get('ignore-chat', {}).get('ignore-before', "1990-01-01")
     ignore_chat_after = config.get('parsing', {}).get('ignore-chat', {}).get('ignore-after', "2150-01-01")
 
     if fileType == InputFileType.INSTAGRAM_EXPORT:
-        return InstagramExport(paths, system_messages, chat_sessions_enabled, sleep_window_start, sleep_window_end,
+        return InstagramExport(chat_sessions_enabled, sleep_window_start, sleep_window_end,
                                ignore_chat_enabled, ignore_chat_before, ignore_chat_after)
     elif fileType == InputFileType.WHATSAPP_EXPORT:
-        return WhatsappExport(paths, system_messages, chat_sessions_enabled, sleep_window_start, sleep_window_end,
+        return WhatsappExport(chat_sessions_enabled, sleep_window_start, sleep_window_end,
                               ignore_chat_enabled, ignore_chat_before, ignore_chat_after)
 
     message = f"Input file type not supported, please choose one of the following: {[e for e in InputFileType]}"
