70490ec974a4f72a08ca0f0c7bd5e46f20fdc975
Manuel <manuelmaterazzo@gmail.com>
2025-06-25 17:42:48 +0200

    refactor

--- src/batch_processor.py
+++ src/batch_processor.py
@@ -8,63 +8,8 @@ from src.service.parser.parser_factory import parser_factory
 from src.service.reader.reader_factory import reader_factory
 from src.service.writer.writer_factory import writer_factory
 
-
 done_count = 1
 total = 0
-
-async def process_single_day(day: str, parser, ai_service, writer, logger):
-    """
-    Processes the diary entry of a single day.
-    :param day:
-    :param parser:
-    :param ai_service:
-    :param writer:
-    :param logger:
-    :return:
-    """
-    global done_count
-    global total
-    try:
-        # get chat log
-        logger.debug(f'Getting chat log for {day}...')
-        chat_log = parser.get_daily_chat_log(day)
-
-        # compute summary (con controllo concorrenza automatico)
-        logger.debug(f'Generating summary for {day}...')
-        summary = await ai_service.get_summary_async(chat_log)
-
-        # write to file
-        logger.debug(f'Writing file for {day}...')
-        writer.write(day, chat_log, summary)
-
-        logger.debug(f'Completed processing for {day}')
-        logger.info(f'{done_count}/{total} done!')
-        done_count = done_count + 1
-        return day
-
-    except Exception as e:
-        logger.error(f'Error processing {day}: {e}')
-        raise
-
-
-async def batch_process_days(day_list: list[str], parser, ai_service, writer, logger):
-    """
-    Processes the diary entry of  multiple days with concurrent processing.
-    """
-    global total
-    total = len(day_list)
-    # Create a task for every day
-    tasks = [process_single_day(day, parser, ai_service, writer, logger) for day in day_list]
-
-    # Execute tasks with parallel processing
-    completed_days = await asyncio.gather(*tasks, return_exceptions=True)
-
-    # Handle exceptions
-    for i, result in enumerate(completed_days):
-        if isinstance(result, Exception):
-            logger.error(f'Failed to process {day_list[i]}: {result}')
-
-    return completed_days
 
 
 def process_all(config: dict):
@@ -113,6 +58,61 @@ def process_all(config: dict):
     ai_service = AiService(config)
 
     # get summary and write each day diary
-    asyncio.run(batch_process_days(day_list, parser, ai_service, writer, logger))
+    asyncio.run(_batch_process_days(day_list, parser, ai_service, writer, logger))
 
     writer.close()
+
+
+async def _batch_process_days(day_list: list[str], parser, ai_service, writer, logger):
+    """
+    Processes the diary entry of  multiple days with concurrent processing.
+    """
+    global total
+    total = len(day_list)
+    # Create a task for every day
+    tasks = [_process_single_day(day, parser, ai_service, writer, logger) for day in day_list]
+
+    # Execute tasks with parallel processing
+    completed_days = await asyncio.gather(*tasks, return_exceptions=True)
+
+    # Handle exceptions
+    for i, result in enumerate(completed_days):
+        if isinstance(result, Exception):
+            logger.error(f'Failed to process {day_list[i]}: {result}')
+
+    return completed_days
+
+
+async def _process_single_day(day: str, parser, ai_service, writer, logger):
+    """
+    Processes the diary entry of a single day.
+    :param day:
+    :param parser:
+    :param ai_service:
+    :param writer:
+    :param logger:
+    :return:
+    """
+    global done_count
+    global total
+    try:
+        # get chat log
+        logger.debug(f'Getting chat log for {day}...')
+        chat_log = parser.get_daily_chat_log(day)
+
+        # compute summary (con controllo concorrenza automatico)
+        logger.debug(f'Generating summary for {day}...')
+        summary = await ai_service.get_summary_async(chat_log)
+
+        # write to file
+        logger.debug(f'Writing file for {day}...')
+        writer.write(day, chat_log, summary)
+
+        logger.debug(f'Completed processing for {day}')
+        logger.info(f'{done_count}/{total} done!')
+        done_count = done_count + 1
+        return day
+
+    except Exception as e:
+        logger.error(f'Error processing {day}: {e}')
+        raise
